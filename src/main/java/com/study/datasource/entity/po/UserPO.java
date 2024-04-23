8b8bcfe304a056f05fac6fe133f4929d9d394979
ayakamisaka1 <2521845542@qq.com>
2024-04-23 15:07:37 +0800

    swagger3.x接入

---
+++ src/main/java/com/study/datasource/entity/po/UserPO.java
@@ -0,0 +1,19 @@
+package com.study.datasource.entity.po;
+
+import lombok.Data;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author : ayaka
+ * @version : 1.0
+ * @appName : DataSource
+ * @moduleName : DataSource
+ * @className : com.study.datasource.entity.po.UserPO
+ * @date : 2024/4/23 14:09
+ */
+@Data
+public class UserPO {
+}
